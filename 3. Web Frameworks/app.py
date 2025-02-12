40706bc02001101c446b4b87652395da1107290e
Mateus Nazário <127886025+MateusNaza@users.noreply.github.com>
2025-02-12 22:18:55 +0000

    feat: Cria API simples com um metodo GET e um POST

---
+++ 3. Web Frameworks/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/saudacao', methods=['GET'])
+def saudacao():
+    nome = request.args.get('nome', 'Amigo')
+    return f'Olá, {nome}! Muito feliz em te ver por aqui! :)'
+
+@app.route('/soma', methods=['POST'])
+def soma():
+    dados = request.get_json()
+    num1 = dados.get('num1', 0)
+    num2 = dados.get('num2', 0)
+    resultado = num1 + num2
+    return jsonify({'resultado': resultado})
+
+if __name__ == '__main__':
+    app.run()
